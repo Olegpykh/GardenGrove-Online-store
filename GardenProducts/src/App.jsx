1162910adea0f91b96d81e0914d7b7e5384828ee
MykytaShcherbakov <shcherbakov.mykyta.s@gmail.com>
2025-06-27 11:11:17 +0200

    Development (#2)
    * Wrong kommit
    
    * Test from Ol 2
    
    ---------
    
    Co-authored-by: Oleg Pykhonin <stoppard23@gmail.com>
    Co-authored-by: Andrii Boiko <andyboy7777777@gmail.com>

--- GardenProducts/src/App.jsx
+++ GardenProducts/src/App.jsx
@@ -8,9 +8,13 @@ function App() {
   return (
     <>
      
+
+    <p>Hallo Bro</p>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
     </>
   )
 }
